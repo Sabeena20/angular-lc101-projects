c54ff64d5c0bc0f3dcd8fe0d67fc5dab5475426a
Sabeena <sabeenarayyan13@gmail.com>
2021-06-21 17:34:15 +0400

    added some code

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://bit.ly/3xCIqWZ';
+  image3 = 'https://bit.ly/3d0Ti9s';
 
   constructor() { }
 
